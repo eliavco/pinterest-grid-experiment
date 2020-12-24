445e36de9d6a74ec8a8f6398697f53a14d62a786
eliavco <eliav.s.cohen@gmail.com>
2020-12-24 18:30:44 +0200

    3 files modified

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  arr;
 
   constructor() { }
 
+  private getSizeArr(n) {
+    var foo = [];
+    for (var i = 1; i <= n; i++) {
+      foo.push(i);
+    }
+    return foo;
+  }
+
   ngOnInit() {
+    this.arr = this.getSizeArr(20);
   }
 
 }
